ace2b16324a1491ac06d4012e7fadb360c9beb38
ji-seung-ryu <8514199@gmail.com>
2021-12-25 06:43:12 +0000

    update_readme

--- README.md
+++
@@ -1,7 +0,0 @@
-**git clone https://github.com/ji-seung-ryu/login-board.git**
-
-**cd login-board**
-
-**service mysql start**
-
-**node app.js** 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-	res.render('index', { title: 'express' });
+	res.render('index', { title: 'login-board' });
 });
 
 router.post('/', function (req,res,next){
